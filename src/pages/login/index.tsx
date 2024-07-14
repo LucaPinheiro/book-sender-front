d50ac622a9c4f9be99ba13852eeb4e8618ee27c9
gabrielmerola <gabrielmerola12@gmail.com>
2024-07-14 00:46:23 -0300

    chore: Update login page with loading state and redirect after successful login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,21 +1,31 @@
-import { Eye, EyeSlash } from '@phosphor-icons/react';
+import { CircleNotch, Eye, EyeSlash,  } from '@phosphor-icons/react';
 import LogoSantander from '../../../public/logoSantander.png';
 import { useState } from 'react';
 
 export function Login() {
     const [showPassword, setShowPassword] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    function HandleLogin() {
+        setLoading(true);
+        setTimeout(() => {
+            alert('Login efetuado com sucesso!');
+            window.location.href = '/email-sender';
+            setLoading(false);
+        }, 1000);
+    }
 
     return (
         <main className="h-screen w-full flex items-center justify-center">
             <section className='flex w-full justify-center h-1/2'>
-                <div className="w-1/3 shadow-xl bg-red-500 rounded-l-lg flex items-center justify-center">
+                <div className="w-1/3 shadow-xl bg-red-500 rounded-l-lg flex items-center justify-center max-lg:hidden">
                     <img
                         src={LogoSantander}
                         alt="Logo do Santander"
                         className="w-64"
                     />
                 </div>
-                <div className="w-1/3 shadow-xl rounded-r-lg p-8 flex flex-col justify-center">
+                <div className="w-1/3 shadow-xl rounded-r-lg p-8 flex flex-col justify-center max-lg:w-1/2 max-sm:w-full">
                     <h1 className="text-3xl font-bold text-center">Login</h1>
                     <form className="flex flex-col gap-4 mt-4">
                         <input className="w-full border-b-2 border-l-2 rounded-bl-md p-2 outline-none border-black" type="email" placeholder="Informe seu email" />
@@ -30,10 +40,18 @@ export function Login() {
                         </div>
 
                         <button
-                            type="submit"
+                            type='button'
                             className="light-effect-button"
+                            disabled={loading}
+                            onClick={()=>HandleLogin()}
                         >
-                            Entrar
+                            {loading ? 
+                                <div className='flex justify-center'>
+                                    <CircleNotch className='animate-spin' size={32} />
+                                </div>
+                            :
+                                <h1>Entrar</h1>
+                            }
                         </button>
                     </form>
                 </div>
