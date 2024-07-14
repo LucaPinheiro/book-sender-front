b169c530277ad81e4d5f404898e63b1b91d9a8e9
gabrielmerola <gabrielmerola12@gmail.com>
2024-07-14 03:33:21 -0300

    refactor: Update EmailRepository to use localStorage token for authorization

--- src/api/repositories/email_repository.ts
+++ src/api/repositories/email_repository.ts
@@ -4,9 +4,28 @@ export class EmailRepository {
 
     async getEmailsByTeam(team: string){
         try {
+            const token = await localStorage.getItem('accessToken');
             const response = await http.get(`/emails/team/${team}`, {
                 headers: {
-                    Authorization: `Bearer ${localStorage.getItem('accessToken')}`
+                    Authorization: `Bearer ${token}`
+                }
+            });
+            return response;
+        } catch (error: any) {
+            throw error.response;
+        }
+    }
+
+    async createEmail(email: string, team: string, role: string){
+        try {
+            const token = await localStorage.getItem('accessToken');
+            const response = await http.post('/create-email', {
+                email,
+                team,
+                role
+            }, {
+                headers: {
+                    Authorization: `Bearer ${token}`
                 }
             });
             return response;
