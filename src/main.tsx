1d83c6ee8a7e95dc75948c4102115eddeb949cbd
gabrielmerola <gabrielmerola12@gmail.com>
2024-07-14 00:46:12 -0300

    feat: Add email sender page with routing support

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './App.tsx'
+import { EmailSender } from './pages/EmailSender'
 import { Login } from './pages/login/index.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-    {/* <Login /> */}
+    <Router>
+      <Routes>
+        <Route path='/' element={<Login/>}/>
+        <Route path='/email-sender' element={<EmailSender />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 )
